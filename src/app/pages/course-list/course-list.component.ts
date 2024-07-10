4c384522cfbb696bd1ea4d4ac87f8d8a291d4778
Saniya Ruth S <saniyaruth07@gmail.com>
2024-07-10 11:15:00 +0530

    Implement course list component with mock data

--- src/app/pages/course-list/course-list.component.ts
+++ src/app/pages/course-list/course-list.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CourseService, Course } from '../../services/course.service';
 
 @Component({
   selector: 'app-course-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './course-list.component.html',
   styleUrl: './course-list.component.css'
 })
-export class CourseListComponent { }
+export class CourseListComponent implements OnInit {
+  courses: Course[] = [];
+  constructor(private courseService: CourseService) {}
+  ngOnInit() { this.courseService.getCourses().subscribe(data => this.courses = data); }
+}
