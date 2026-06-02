fc48d5a7c3a6e228bbcd1a9ad97d86fe5cf4bcf9
SaniyaRuthS <saniyaruth07@gmail.com>
2026-06-02 22:54:02 +0530

    Added project files

---
+++ JavaExercise15.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class JavaExercise15 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter string: ");
+        String str = scanner.nextLine();
+        String reversed = new StringBuilder(str).reverse().toString();
+        System.out.println(reversed);
+    }
+}
---
+++ SQL EXERCISE 6.sql
@@ -0,0 +1,4 @@
+SELECT e.title, r.resource_type, COUNT(r.resource_id) AS resource_count
+FROM Events e
+LEFT JOIN Resources r ON e.event_id = r.event_id
+GROUP BY e.event_id, e.title, r.resource_type;
