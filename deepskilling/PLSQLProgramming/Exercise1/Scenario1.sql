bec03c09a8484fd0e5cd4d429d300b6d810daa5e
SaniyaRuthS <saniyaruth07@gmail.com>
2026-06-22 20:44:41 +0530

    Convert Exercise1 to normal folder

---
+++ deepskilling/PLSQLProgramming/Exercise1/Scenario1.sql
@@ -0,0 +1,19 @@
+BEGIN
+    FOR rec IN (
+        SELECT c.CustomerID, l.LoanID, l.InterestRate
+        FROM Customers c
+        JOIN Loans l
+        ON c.CustomerID = l.CustomerID
+        WHERE c.Age > 60
+    )
+    LOOP
+        UPDATE Loans
+        SET InterestRate = InterestRate - 1
+        WHERE LoanID = rec.LoanID;
+
+        DBMS_OUTPUT.PUT_LINE('Discount applied to Customer ID: ' || rec.CustomerID);
+    END LOOP;
+
+    COMMIT;
+END;
+/
