5649fcdc8b9e576405ed46e502b177b264635756
SaniyaRuthS <saniyaruth07@gmail.com>
2026-07-21 22:36:39 +0530

    Completed Ticket Booking App

---
+++ deepskilling/React/12. ReactJS-HOL/src/components/Greeting.js
@@ -0,0 +1,5 @@
+import UserGreeting from './UserGreeting';
+import GuestGreeting from './GuestGreeting';
+export default function Greeting(props){
+return props.isLoggedIn ? <UserGreeting/> : <GuestGreeting/>;
+}
