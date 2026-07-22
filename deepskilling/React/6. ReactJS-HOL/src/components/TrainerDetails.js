a1564f0f7f75da94fea3f1a6af5e39b9dc50fd87
SaniyaRuthS <saniyaruth07@gmail.com>
2026-07-22 09:29:10 +0530

    Completed TrainersApp React Router Handson

---
+++ deepskilling/React/6. ReactJS-HOL/src/components/TrainerDetails.js
@@ -0,0 +1,1 @@
+import {useParams} from 'react-router-dom'; import trainers from '../TrainersMock'; export default function TrainerDetails(){const {id}=useParams(); const t=trainers.find(x=>x.trainerId===id); return <div><h2>Trainers Details</h2><h3>{t.name} ({t.technology})</h3><p>{t.email}</p><p>{t.phone}</p><ul>{t.skills.map((s,i)=><li key={i}>{s}</li>)}</ul></div>}
